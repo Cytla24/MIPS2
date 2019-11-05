fb8143e02686da1ec0e1ee74d0d1ba5ff10abed4
Cytla24 <ojuba2013@gmail.com>
2019-11-05 00:51:18 -0500

    calculated M and N

--- project2.s
+++ project2.s
@@ -11,3 +11,9 @@ main:
 	ori $t8, $t8, 19715
 	add $t0, $zero, $t8				# int $t0 = 02837763
 	addi $t1, $zero, 11				# int $t1 = 11
+	div $t0, $t1					# divide t0/t1
+	mfhi $t3						# remainder = t3
+	addi $s0, $t3, 26				# N = s0
+	addi $t9, $zero, 10
+	sub $s1, $s0, $t9				# M = s1
+	
